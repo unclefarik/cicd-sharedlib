5919b1ce62c6403b71d74388d11a59ab549e25f1
Farkhat Baterikov <farkhat.baterikov@Farkhats-MacBook-Pro.local>
2025-10-18 09:33:52 +0500

    fix:trivy shared dir

--- vars/dockerOps.groovy
+++ vars/dockerOps.groovy
@@ -30,7 +30,7 @@ def scanImage(String imageRef) {
     echo "Scanning ${imageRef} for vulnerabilities..."
     sh """
         docker run --rm \
-          -v \$HOME/.cache/trivy:/root/.cache/ \
+          -v /tmp/trivy-cache:/root/.cache/ \
           -v /var/run/docker.sock:/var/run/docker.sock \
           aquasec/trivy:latest image \
           --exit-code 1 \
